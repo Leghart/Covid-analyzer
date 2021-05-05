a2df3695ff40bc262b11d74be54bce7d8044c733
Dawid <leghart@wp.pl>
2021-05-05 14:05:43 +0200

    PEP8

--- Data_Base.py
+++ Data_Base.py
@@ -1,24 +1,34 @@
-from lib import *
+import sqlite3
+import pandas as pd
 
-class DB:
 
-    def __init__(self,country):
-        self.con=sqlite3.connect('Covid_Data.db')
-        self.cursor=self.con.cursor()
-        self.country=country
-        # check existing table
-        self.cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='"+self.country+"' ")
+class DataBase:
 
-        if self.cursor.fetchone()[0]!=1:
-            self.cursor.execute("CREATE TABLE "+self.country+"(date TEXT, new_cases INT, total_cases INT, new_deaths INT, total_deaths INT, total_recovered INT, active_cases INT, tot_1M REAL, fatality_ratio REAL, total_tests INT)")
+    def __init__(self, country):
+        self.con = sqlite3.connect('Covid_Data.db')
+        self.cursor = self.con.cursor()
+        self.country = country
+
+        self.cursor.execute(
+                """SELECT count(name) FROM sqlite_master WHERE
+                type='table' AND name='""" + self.country + "' ")
+
+        if self.cursor.fetchone()[0] != 1:
+            self.cursor.execute("CREATE TABLE " + self.country +
+                                """(date TEXT, new_cases INT,
+                                total_cases INT, new_deaths INT,
+                                total_deaths INT, total_recovered INT,
+                                active_cases INT, tot_1M REAL,
+                                fatality_ratio REAL, total_tests INT)""")
+
             logf = open("log.txt", "a")
             logf.write(f'Table not existing. Created table: {self.country}.\n')
             logf.close()
         else:
             print("successful database connection.\n")
 
-    def commit(self,S):
-        logf=open('log.txt','a')
+    def commit(self, S):
+        logf = open('log.txt', 'a')
         try:
             self.con.commit()
             self.cursor.close()
@@ -30,28 +40,40 @@ class DB:
             logf.close()
 
     def get_last_record_date(self):
-        self.cursor.execute("SELECT date FROM "+self.country )
+        self.cursor.execute("SELECT date FROM " + self.country)
         try:
-            last_date=self.cursor.fetchall()[-1][0]
+            last_date = self.cursor.fetchall()[-1][0]
         except Exception:
-            last_date=None
+            last_date = None
         return last_date
 
     def get_actual_data(self):
-        self.cursor.execute("SELECT * FROM "+self.country )
-        data=self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM " + self.country)
+        data = self.cursor.fetchall()
 
-        df=pd.DataFrame(list(data))
-        df=df.set_axis(['Date','New cases','Total cases','New deaths','Total deaths,','Total recovered','Active cases','Tot /1M','Fatality ratio','Total_tests'],axis='columns')
-        df=df.drop(columns=['Data'])
+        df = pd.DataFrame(list(data))
+        df = df.set_axis(['Date', 'New cases',
+                         'Total cases', 'New deaths', 'Total deaths,',
+                          'Total recovered', 'Active cases', 'Tot /1M',
+                          'Fatality ratio', 'Total_tests'], axis='columns')
+        df = df.drop(columns=['Data'])
         return df
 
-    def insert(self,S):
-        logf=open('log.txt','a')
-        file=open('raports.txt','a')
-        if self.get_last_record_date()!=S.date:
-            self.cursor.execute("INSERT INTO "+self.country+ " VALUES (?,?,?,?,?,?,?,?,?,?)",(S.date, S.new_cases, S.total_cases, S.new_deaths, S.total_deaths, S.total_rec, S.active_cases, S.tot, S.fatality_ratio,S.total_tests))
-            file.write(str(self.country)+ ' ' +str(S.date)+' '+str(S.new_cases)+' '+str(S.total_cases)+' '+str(S.new_deaths)+' '+str(S.total_deaths)+' '+str(S.total_rec)+ ' '+str(S.active_cases)+' '+str(S.tot)+' '+str(S.fatality_ratio)+' '+str(S.total_tests)+'\n')
+    def insert(self, S):
+        logf = open('log.txt', 'a')
+        file = open('raports.txt', 'a')
+        if self.get_last_record_date() != S.date:
+            self.cursor.execute("INSERT INTO " + self.country +
+                                " VALUES (?,?,?,?,?,?,?,?,?,?)",
+                                (S.date, S.new_cases, S.total_cases,
+                                 S.new_deaths, S.total_deaths, S.total_rec,
+                                 S.active_cases, S.tot, S.fatality_ratio,
+                                 S.total_tests))
+            # file.write(str(self.country)+ ' ' +str(S.date)+' '+str(S.new_cases)+' '+str(S.total_cases)+' '+str(S.new_deaths)+' '+str(S.total_deaths)+' '+str(S.total_rec)+ ' '+str(S.active_cases)+' '+str(S.tot)+' '+str(S.fatality_ratio)+' '+str(S.total_tests)+'\n')
+            file.write('{} {} {} {} {} {} {} {} {} {} {} \n'.format(
+                self.country, S.date, S.new_cases, S.total_cases,
+                S.new_deaths, S.total_deaths, S.total_rec, S.active_cases,
+                S.tot, S.fatality_ratio, S.total_tests))
             self.commit(S)
         else:
             logf.write(f"That record is already in data base ({S.date}).\n")
--- Scrap.py
+++ Scrap.py
@@ -1,64 +1,69 @@
-from lib import *
-from Data_Base import DB
+import textwrap
+from bs4 import BeautifulSoup
+from requests import get
+from datetime import date
+import ssl
+import smtplib
 
-
+from data_base import DataBase as DB
+from processing import Process as P
 
 def format_number(string):
-    return  " ".join(digit for digit in textwrap.wrap(str(string)[::-1], 3))[::-1]
+    return " ".join(digit for digit in textwrap.wrap(
+                                str(string)[::-1], 3))[::-1]
 
 
-class Daily_Raport:
+class DailyRaport:
 
-    def __init__(self,country):
+    def __init__(self, country):
         self.get_actual_data(country)
 
-    def get_actual_data(self,country):
-        url='https://www.worldometers.info/coronavirus/#main_table'
-        page=get(url)
-        bs=BeautifulSoup(page.content,'html.parser')
-        data=''
+    def get_actual_data(self, country):
+        url = 'https://www.worldometers.info/coronavirus/#main_table'
+        page = get(url)
+        bs = BeautifulSoup(page.content, 'html.parser')
+        data = ''
 
-        table_data=bs.find('table',id='main_table_countries_today')
+        table_data = bs.find('table', id='main_table_countries_today')
 
-        data=''
+        data = ''
         for tr in table_data.find_all('tr'):
-            data+=tr.get_text()
+            data += tr.get_text()
 
-        data=data.split('\n')
-        idx=data.index(country)
+        data = data.split('\n')
+        idx = data.index(country)
 
-        end_idx=idx+14
-        data=data[idx:end_idx]
+        end_idx = idx + 14
+        data = data[idx:end_idx]
 
         try:
-            self.country=data[0]
-            self.total_cases=int(data[1].replace(',',''))
-            self.new_cases=int(data[2].replace('+','').replace(',',''))
-            self.total_deaths=int(data[3].replace(',',''))
-            self.new_deaths=int(data[4].replace('+','').replace(',',''))
-            self.total_rec=int(data[5].replace(',',''))
-            self.active_cases=int(data[7].replace('+','').replace(',',''))
-            self.tot=int(data[9].replace('+','').replace(',','')) #na 1M
-            self.total_tests=int(data[11].replace(',',''))
-            self.fatality_ratio=round(self.total_deaths/self.total_cases*100,2)
+            self.country = data[0]
+            self.total_cases = int(data[1].replace(',', ''))
+            self.new_cases = int(data[2].replace('+', '').replace(',', ''))
+            self.total_deaths = int(data[3].replace(',', ''))
+            self.new_deaths = int(data[4].replace('+', '').replace(',', ''))
+            self.total_rec = int(data[5].replace(',', ''))
+            self.active_cases = int(data[7].replace('+', '').replace(',', ''))
+            self.tot = int(data[9].replace('+', '').replace(',', ''))
+            self.total_tests = int(data[11].replace(',', ''))
+            self.fatality_ratio = round(self.total_deaths /
+                                        self.total_cases * 100, 2)
 
-
-            date_today=date.today()
-            self.date=date_today.strftime("%d.%m.%Y")
+            date_today = date.today()
+            self.date = date_today.strftime("%d.%m.%Y")
 
         except ValueError:
             print(f"Data wasn't uploaded on page yet ({self.country}).\n")
             exit(-1)
 
-    def save_data_to_csv(self,ofile,nfile,country):
-        nowy_plik=open(nfile,'w')
+    def save_data_to_csv(self, ofile, nfile, country):
+        out_file = open(nfile, 'w')
         for filename in os.listdir(ofile):
-           with open(os.path.join(ofile, filename), 'r') as f: # open in readonly mode
-              plik=f.read()
-              linie=plik.split('\n')
-              wynik=[s for s in linie if country in s]
-              print(wynik)
-              nowy_plik.writelines(["%s\n" % item  for item in wynik])
+            with open(os.path.join(ofile, filename), 'r') as f:
+                day = f.read()
+                lines = day.split('\n')
+                out = [s for s in lines if country in s]
+                out_file.writelines(["%s\n" % item for item in out])
 
     def show_raport(self):
         print(f'Country: {self.country}')
@@ -74,9 +79,9 @@ class Daily_Raport:
         print(f'Data recived: {self.date}')
 
     def raport_to_mail(self):
-        From="Automatyczny Raport Wirusowy"
-        subject=f'Raport z dnia: {self.date}'
-        message ="""Wszystkie przypadki zachorowan: {}\n
+        From = "Automatyczny Raport Wirusowy"
+        subject = f'Raport z dnia: {self.date}'
+        message = """Wszystkie przypadki zachorowan: {}\n
         Dzisiejsze zachorowania: {}\n
         Wszystkie zgony: {}\n
         Dzisiejsze zgony: {}\n
@@ -85,55 +90,39 @@ class Daily_Raport:
         Ilość zmarłych na 1M: {}\n
         Współczynnik smiertelnosci: {}\n
         Wszystkie wykonane testy: {}
-        """.format(format_number(str(self.total_cases)),format_number(str(self.new_cases)),format_number(str(self.total_deaths)),format_number(str(self.new_deaths)),format_number(str(self.total_rec)),format_number(str(self.active_cases)),format_number(str(self.tot)),format_number(str(self.fatality_ratio)),format_number(str(self.total_cases)))
+        """.format(
+                    format_number(str(self.total_cases)),
+                    format_number(str(self.new_cases)),
+                    format_number(str(self.total_deaths)),
+                    format_number(str(self.new_deaths)),
+                    format_number(str(self.total_rec)),
+                    format_number(str(self.active_cases)),
+                    format_number(str(self.tot)),
+                    format_number(str(self.fatality_ratio)),
+                    format_number(str(self.total_cases)))
 
-        return 'Subject: {}\n\n{}'.format(subject,message.encode('ascii', 'ignore').decode('ascii'))
+        return 'Subject: {}\n\n{}'.format(subject, message.encode(
+                                    'ascii', 'ignore').decode('ascii'))
 
     def send_mail(self):
-        port=465
-        file=open('passwords')
-        passw=file.read().split(';')
-        smtp_server="smtp.gmail.com"
-        nadawca=passw[0]
-        odbiorca=passw[1]
-        haslo=passw[2]
+        port = 465
+        file = open('passwords')
+        passw = file.read().split(';')
+        smtp_server = "smtp.gmail.com"
+        broadcaster = passw[0]
+        receiver = passw[1]
+        pass_ = passw[2]
 
-        message=self.raport_to_mail()
+        message = self.raport_to_mail()
 
-        ssl_pol=ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server,port,context=ssl_pol) as serwer:
-            serwer.login(nadawca,haslo)
-            serwer.sendmail(nadawca,odbiorca,message)
-
-
+        ssl_pol = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=ssl_pol) as serwer:
+            serwer.login(broadcaster, pass_)
+            serwer.sendmail(broadcaster, receiver, message)
 
 
-D=Daily_Raport('Poland')
+D = DailyRaport('Poland')
 D.show_raport()
-W=DB('Poland')
+W = DB('Poland')
 W.insert(D)
 D.send_mail()
-
-
-
-'''
-def get_vacc_data(self):
-    url='https://www.medonet.pl/zdrowie/zdrowie-dla-kazdego,zasieg-koronawirusa-covid-19--mapa-,artykul,43602150.html'
-    page=get(url)
-    bs=BeautifulSoup(page.content,'html.parser')
-    data=''
-
-    for i in bs.find_all('div',class_='inlineFrame'):
-        data+=str(i.get_text())+'\n'
-
-    data=[x for x in data.splitlines() if x]
-
-    nvacc=data[4].replace(' ','')
-    tvacc=data[1].replace(' ','')
-    ntests=data[7].replace(',','.')
-
-    self.new_vaccinated=int(nvacc.split(':')[1])
-    tmp_tests=ntests.split(':')[1]
-    n=tmp_tests.split(' ')
-    self.new_tests=float(n[1])*1000
-'''
