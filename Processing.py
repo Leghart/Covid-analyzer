ef781894f6466ffb65165e28c12b9992b38ebd65
Dawid <leghart@wp.pl>
2021-06-17 19:47:33 +0200

    Dekorator plotów, ARIMA, HoltWinters, VAR,

--- Processing.py
+++ Processing.py
@@ -13,9 +13,22 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV
+from scipy.interpolate import Rbf
+from sklearn.metrics import r2_score
 
-from data_base import db_session, get_last_record, insert, delete, get_data
-from data_base import PredBase, MainBase, init_db
+from pmdarima.arima import auto_arima
+from pmdarima.arima import ADFTest
+
+from data_base import init_db
+from data_base import MainBase
+from data_base import PredBase
+
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
 
 
 def format_number(string):
@@ -29,7 +42,7 @@ class Process:
                 'total_tests', 'date']
 
     def __init__(self):
-        raw_data = get_data(MainBase)
+        raw_data = MainBase.get_data()
 
         for key in __class__.fields:
             self.__dict__[key] = []
@@ -49,7 +62,7 @@ class Process:
             Y.append(y_i)
         return np.array(X), np.array(Y)
 
-    def get_data(self):
+    def get_data_from_self(self):
         new_data = np.arange(len(self.date))
         d = {'Date': new_data, 'Total cases': self.total_cases,
              'New cases': self.new_cases, 'Total deaths': self.total_deaths,
@@ -61,39 +74,6 @@ class Process:
         df = pd.DataFrame(data=d)
         df = df.drop(columns=['Tot /1M', 'Total tests'])
         return df
-
-    def mlp_regress(self, key):
-        X, y = Process.preprocessData(self.get_data(), key)
-        X = X.astype(np.int)
-        y = y.astype(np.int)
-        y = np.ravel(y)
-
-        sc = MinMaxScaler()
-        X_sc = sc.fit_transform(X)
-
-        X_train, X_test, y_train, y_test = train_test_split(X_sc, y,
-                                                            test_size=0.2,
-                                                            random_state=0)
-
-        mlp = MLPRegressor(hidden_layer_sizes=(100,), activation='tanh',
-                           solver='sgd', learning_rate='adaptive',
-                           learning_rate_init=0.0001, max_iter=1000)
-
-        mlp.fit(X_train, y_train)
-        t = np.arange(0, len(y), 1).reshape(-1, 1)
-
-        yp = mlp.predict(X_sc)
-        print(f'{i}: {yp[-1]}')
-
-        fig = plt.figure(figsize=(10, 8))
-        ax1 = fig.add_subplot(111)
-        plt.plot(t, y, 'r', label='oryginalne dane')
-        plt.plot(t, yp, 'b', label='predykcja')
-        plt.xlabel('Numer próbki')
-        plt.ylabel(key)
-        plt.grid(which='both')
-        plt.legend()
-        plt.show()
 
     def Kohonen(self, X, klasy, alfa=0.45, il_iter=100):
         srd = sum(i for i in X) / len(X)
@@ -160,11 +140,13 @@ class Process:
         return y_rad
 
     def RBF_prediction(self, keys):
-        data = self.get_data()
+        data = self.get_data_from_self()
+        data = data[:-1]
+
         pred_dict = {}
         for i in keys:
             plt.figure(i)
-            X, y = Process.preprocessData(data, i, 75)
+            X, y = Process.preprocessData(data, i, int(len(self.date)*0.2))
             print('Calculating prediction for {}'.format(i))
             y_rad = self.RBF(X, y, 200, 10)
 
@@ -185,6 +167,50 @@ class Process:
                 os.remove(full_path)
 
             plt.savefig(Process.path + '\static/{}'.format(i))
+        self.cases_pred = pred_dict['New cases']
+        self.deaths_pred = pred_dict['New deaths']
+
+        self.next_day = (datetime.datetime.strptime(self.date[-1],'%d.%m.%Y') +
+                    datetime.timedelta(days=1)).strftime('%d.%m.%Y')
+
+    def plot_decorator(f):
+        def func(self, *args):
+            kwargs = f(self, *args)
+            for key in kwargs:
+                plt.figure(key)
+                plt.plot(kwargs[key][0], label="Original data")
+                plt.plot(kwargs[key][1], label="Forecast")
+                plt.legend()
+                plt.title('{} ({})'.format(key, self.date[-1]))
+                plt.grid()
+
+                full_path = __class__.path + '\static\{}.png'.format(key)
+                if os.path.isfile(full_path):
+                    os.remove(full_path)
+
+                plt.savefig(__class__.path + '\static/{}'.format(key))
+
+        return func
+
+    @plot_decorator
+    def ARIMA(self, keys, days_pred=7):
+        pred_dict = {}
+        kwargs = {}
+        for key in keys:
+            data = self.get_data_from_self()[key]
+            act = len(data)
+            horizont = pd.DataFrame(np.zeros(days_pred))
+            ndata = pd.concat([data, horizont], ignore_index=True)
+
+            print('Calculating prediction for {}'.format(key))
+            arima_model =  auto_arima(data[:act], method='lbfgs')
+            test = ndata[act:]
+
+            prediction = pd.DataFrame(arima_model.predict(n_periods = len(test)), index=test.index)
+            print(prediction)
+            pred_dict[key] = int(prediction.values.tolist()[0][0])
+            kwargs[key] = [data, prediction]
+
 
         self.cases_pred = pred_dict['New cases']
         self.deaths_pred = pred_dict['New deaths']
@@ -195,22 +221,14 @@ class Process:
         init_db()
         cap = {'date': self.next_day,'cases_pred': self.cases_pred,
                 'deaths_pred': self.deaths_pred}
-        insert(PredBase, **cap)
+        PredBase.insert(**cap)
 
-    def save_predicion_to_txt(self, date, file):
-        text = '''Prediction for {}: new cases: {}
-                           new deaths: {}\n'''.format(date,
-                                        self.cases_pred,
-                                        self.deaths_pred)
-        with open(file, 'a', encoding='utf-8') as f:
-            f.write(text)
-        print(text)
-        print('Successfully written to file!')
+        return kwargs
 
     def raport_to_mail(self):
         From = "Automatyczny Raport Wirusowy"
         subject = f'Raport z dnia: {self.date[-1]}'
-        message = """
+        message = """\n
 
         Dzisiejsze zachorowania: {}\n
         Dzisiejsze zgony: {}\n
@@ -241,28 +259,139 @@ class Process:
         return 'Subject: {}\n\n{}'.format(subject, message.encode(
                                     'ascii', 'ignore').decode('ascii'))
 
-    def send_mail(self, path_file):
-        port = 465
-        try:
-            file = open(path_file)
-            passw = file.read().split(';')
-            smtp_server = "smtp.gmail.com"
-            broadcaster = passw[0]
-            receiver = passw[1]
-            pass_ = passw[2]
+    def send_mail(self, broadcaster_handler, receiver_handler,
+                  password_handler):
 
+        port = 465
+        smtp_server = "smtp.gmail.com"
+        try:
+            broadcaster = open(broadcaster_handler).read()
+            receiver = open(receiver_handler).read().split(';')
+            password = open(password_handler).read()
             message = self.raport_to_mail()
+            del receiver[-1]
 
             ssl_pol = ssl.create_default_context()
             with smtplib.SMTP_SSL(smtp_server, port, context=ssl_pol) as serwer:
-                serwer.login(broadcaster, pass_)
+                serwer.login(broadcaster, password)
                 serwer.sendmail(broadcaster, receiver, message)
             print('Mail was sent!')
         except Exception as e:
             print('Error:', e)
 
-'''
-P = Process()
-P.RBF_prediction(['New cases','New deaths'])
-print(P.cases_pred, P.deaths_pred, P.next_day)
-'''
+    def ARIMA_test(self, keys, days_pred=7):
+        pred_dict = {}
+        kwargs = {}
+        for key in keys:
+            data = self.get_data_from_self()[key]
+            act = len(data)
+            horizont = pd.DataFrame(np.zeros(days_pred))
+            ndata = pd.concat([data, horizont], ignore_index=True)
+            arima_model =  auto_arima(data[:act], method='lbfgs')
+            test = ndata[act:]
+
+            prediction = pd.DataFrame(arima_model.predict(n_periods = len(test)), index=test.index)
+            #prediction.columns = ['prediction']
+            pred_dict[key] = int(prediction.values.tolist()[0][0])
+            kwargs[key] = [data, prediction]
+
+        return kwargs
+
+    def VAR(self, keys=['New cases','New deaths']):
+        from statsmodels.tsa.vector_ar.var_model import VAR
+        # contrived dataset with dependency
+        data1 = self.get_data_from_self()[keys[0]]
+        data2 = self.get_data_from_self()[keys[1]]
+
+        data = list()
+        for i in range(len(data1)):
+            v1 = data1[i]
+            v2 = data2[i]
+
+            row = [v1, v2]
+            data.append(row)
+
+        # fit model
+        #print(data[-3:])
+        model = VAR(data)
+        model_fit = model.fit()
+        # make prediction
+        yhat = model_fit.forecast(model_fit.y, steps=1)
+        #print('{} - predykcja: {}'.format(VAR.__name__, yhat ))
+        dict ={keys[0]: int(yhat[0][0]), keys[1]: int(yhat[0][1])}
+        return dict
+
+    def HVES(self, keys):
+        from statsmodels.tsa.holtwinters import ExponentialSmoothing as HWES
+        di = {}
+        for k in keys:
+            df = self.get_data_from_self()[k]
+            df = df[10:]
+
+            tmp = 3
+            df_train = df.iloc[:-tmp]
+            df_test = df.iloc[-tmp:]
+            model = HWES(df_train, seasonal_periods=tmp, trend='mul', seasonal='add', initialization_method='estimated')
+            #fitted = model.fit(optimized=True, use_brute=True)
+            fitted = model.fit()
+            #print out the training summary
+            #print(fitted.summary())
+
+            #create an out of sample forcast for the next 12 steps beyond the final data point in the training data set
+            sales_forecast = fitted.forecast(steps=tmp)
+
+            predi = list(sales_forecast[:1])
+            di[k] = int(predi[0])
+        #print('{} - predykcja : {}'.format('HVES', di ))
+        return di
+
+        '''
+        fig = plt.figure()
+        fig.suptitle('Retail Sales of Used Cars in the US (1992-2020)')
+        past, = plt.plot(df_train.index, df_train, 'b.-', label='Sales History')
+        future, = plt.plot(df_test.index, df_test, 'r.-', label='Actual Sales')
+        predicted_future, = plt.plot(df_test.index, sales_forecast, 'g.-', label='Sales Forecast')
+        plt.legend(handles=[past, predicted_future])
+        plt.show()
+        '''
+
+    def PRED(self, keys=['New cases', 'New deaths'], Forecast_hor=7):
+
+        next_day = (datetime.datetime.strptime(MainBase.get_data()[-1]['date'],'%d.%m.%Y') +
+                    datetime.timedelta(days=1)).strftime('%d.%m.%Y')
+
+        print('Predykcja na {}'.format(next_day) )
+        #v = self.VAR(keys)
+        #print('VAR: ', v)
+
+        a = self.ARIMA_test(keys, Forecast_hor)
+        print('ARIMA: ', a)
+
+        h = self.HVES(keys)
+        print('HVES: ', h)
+
+        waga = 3
+        cases = int(( a['New cases'] + h['New cases'])/waga)
+        deaths = int(( + a['New deaths'] + h['New deaths'])/waga)
+
+        print('TOTAL: ',cases,deaths)
+
+
+        self.cases_pred = cases
+        self.deaths_pred = deaths
+        self.next_day = next_day
+
+
+        init_db()
+        cap = {'date': self.next_day,'cases_pred': self.cases_pred,
+                'deaths_pred': self.deaths_pred}
+        PredBase.insert(**cap)
+
+
+#P = Process()
+#P.ARIMA_test(['New cases','New deaths'])
+
+# P.PRED()
+# cap = {'date': '69-69-69','cases_pred': 10,
+#     'deaths_pred': 20}
+# PredBase.insert(**cap)
