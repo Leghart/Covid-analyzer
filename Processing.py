3649e6399ff2c48d1eb49fc1d44ca3a9e083f257
Dawid <leghart@wp.pl>
2021-06-18 16:24:07 +0200

    lstm-train,test

--- Processing.py
+++ Processing.py
@@ -29,11 +29,24 @@ import warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def format_number(string):
     return " ".join(digit for digit in textwrap.wrap(
                                 str(string)[::-1], 3))[::-1]
+
+
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+
+
 
 class Process:
     path = os.path.dirname(os.path.abspath(__file__))
@@ -388,7 +401,72 @@ class Process:
         PredBase.insert(**cap)
 
 
-#P = Process()
+    def LSTM(self, keys=0):
+        from tensorflow.keras.models import Sequential
+        from tensorflow.keras.layers import Dense
+        from tensorflow.keras.layers import LSTM
+        from sklearn.preprocessing import MinMaxScaler
+        from sklearn.metrics import mean_squared_error
+
+        key = 'New cases'
+        data = self.get_data_from_self()[key].values
+        data = data.reshape(len(data),1)
+
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        dataset = scaler.fit_transform(data)
+
+
+#       split into train and test sets
+        train_size = int(len(dataset) * 0.9)
+        test_size = len(dataset) - train_size
+        train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+        look_back = 10
+        trainX, trainY = create_dataset(train, look_back)
+        testX, testY = create_dataset(test, look_back)
+
+        trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+        testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+        model = Sequential()
+        model.add(LSTM(4, input_shape=(1, look_back)))
+        model.add(Dense(1))
+        model.compile(loss='mean_squared_error', optimizer='nadam')
+        model.fit(trainX, trainY, epochs=500, batch_size=32, verbose=2)
+
+
+        trainPredict = model.predict(trainX)
+        testPredict = model.predict(testX)
+        # invert predictions
+        trainPredict = scaler.inverse_transform(trainPredict)
+        trainY = scaler.inverse_transform([trainY])
+        testPredict = scaler.inverse_transform(testPredict)
+        testY = scaler.inverse_transform([testY])
+        # calculate root mean squared error
+        trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+        #print('Train Score: %.2f RMSE' % (trainScore))
+        testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+        #print('Test Score: %.2f RMSE' % (testScore))
+
+
+
+        trainPredictPlot = np.empty_like(dataset)
+        trainPredictPlot[:, :] = np.nan
+        trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+        # shift test predictions for plotting
+        testPredictPlot = np.empty_like(dataset)
+        testPredictPlot[:, :] = np.nan
+        testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+        # plot baseline and predictions
+        plt.plot(scaler.inverse_transform(dataset))
+        plt.plot(trainPredictPlot)
+        plt.plot(testPredictPlot)
+        plt.show()
+
+
+
+P = Process()
+P.LSTM()
 #P.ARIMA_test(['New cases','New deaths'])
 
 # P.PRED()
