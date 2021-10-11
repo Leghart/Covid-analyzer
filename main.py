8960a2286a37e7149fb0a529dd9f197b69f975e2
Dawid <dawid.sieluzycki@nokia.com>
2021-10-11 20:50:10 +0200

    linux upgrade part1

--- main.py
+++ main.py
@@ -2,27 +2,25 @@ import datetime
 import os
 import time
 
+from data_base import MainBase, PredBase
 from processing import Process
-from scrap import DailyReport as DR
 from scrap import CollectDataException
+from scrap import DailyReport as DR
+from setup import Country, Forecast_hor, scrap_time
 
-from data_base import PredBase, MainBase
-from setup import Country, scrap_time, Forecast_hor
+if __name__ == "__main__":
+    today_ = datetime.datetime.today().strftime("%d.%m.%Y")
+    keys = ["New cases", "New deaths", "New recovered"]
 
-
-if __name__ == '__main__':
-    today_ = datetime.datetime.today().strftime('%d.%m.%Y')
-    keys = ['New cases', 'New deaths', 'New recovered']
-
-    while(True):
+    while True:
         hour_now = datetime.datetime.now().hour
         min_now = datetime.datetime.now().minute
-        time_now = str(hour_now) + ':' + str(min_now)
+        time_now = str(hour_now) + ":" + str(min_now)
         try:
             if time_now >= scrap_time:
                 if MainBase.get_last_record(get_date=True) != today_:
-                    os.system('cls')
-                    print('Today pred: {}'.format(PredBase.get_last_record()))
+                    os.system("cls")
+                    print("Today pred: {}".format(PredBase.get_last_record()))
                     D = DR(Country)
                     print(D)
 
@@ -31,13 +29,20 @@ if __name__ == '__main__':
                     MainBase.insert(**kwargs)
 
                     Pl = Process()
-                    Pl.ARIMA(keys=keys, days_pred=Forecast_hor,
-                             config_plot=True, config_db=True)
+                    Pl.ARIMA(
+                        keys=keys,
+                        days_pred=Forecast_hor,
+                        config_plot=True,
+                        config_db=True,
+                    )
 
-                    broad_file = Pl.path + r'\broadcaster'
-                    rec_file = Pl.path + r'\receiver'
-                    pass_file = Pl.path + r'\password'
-                    Pl.send_mail(broad_file, rec_file, pass_file)
+                    # broad_file = Pl.path + r'\broadcaster'
+                    # rec_file = Pl.path + r'\receiver'
+                    # pass_file = Pl.path + r'\password'
+                    # broad_file = Pl.path + "/broadcaster"
+                    # rec_file = Pl.path + "/receiver"
+                    # pass_file = Pl.path + "/password"
+                    # Pl.send_mail(broad_file, rec_file, pass_file)
                     exit()
 
         except CollectDataException as e:
